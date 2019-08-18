cc603e2247daa43e6763eefc5c04a2259090845b
aakashrajput <aakashsingh1999@gmail.com>
2019-08-18 19:27:25 +0530

    tutor page update

--- tutors.php
+++ tutors.php
@@ -77,6 +77,13 @@
                                                             </figure>
                                                         </a>
                                                     </li>
+                                                    <li class="wow zoomIn" data-wow-duration="1s" data-wow-delay=".7s">
+                                                        <a href="#team-5" data-team="team-5">
+                                                            <figure>
+                                                                <img src="./img/avinash.jpg" alt="Team Member image four">
+                                                            </figure>
+                                                        </a>
+                                                    </li>
                                                 </ul>
                                             </div>
                                             <!-- END team-list-->
@@ -89,10 +96,10 @@
                                                     <img src="./img/shivay.jpg" alt="">
                                                     <div class="team-social">
                                                         <ul>
-                                                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-github"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                                            <li><a href="https://m.facebook.com/harry.slpotter?ref=bookmarks"><i class="fa fa-facebook"></i></a></li>
+                                                            <li><a href="https://twitter.com/HowDevelop"><i class="fa fa-twitter"></i></a></li>
+                                                            <li><a href="https://github.com/"><i class="fa fa-github"></i></a></li>
+                                                            <li><a href="https://LinkedIn.com/in/Shivaylamba/"><i class="fa fa-linkedin"></i></a></li>
                                                         </ul>
                                                     </div>
                                                     <!-- END team-social-->
@@ -114,8 +121,8 @@
                                                         <ul>
                                                             <li><a href=""><i class="fa fa-facebook"></i></a></li>
                                                             <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-github"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                                            <li><a href="https://GitHub.com/shivam123425"><i class="fa fa-github"></i></a></li>
+                                                            <li><a href="https://www.linkedin.com/in/shivam-kumar-85a660164/"><i class="fa fa-linkedin"></i></a></li>
                                                         </ul>
                                                     </div>
                                                     <!-- END team-social-->
@@ -135,10 +142,10 @@
                                                     <img src="./img/piyush.jpg" alt="">
                                                     <div class="team-social">
                                                         <ul>
-                                                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                                            <li><a href="https://www.facebook.com/profile.php?id=100002501443596"><i class="fa fa-facebook"></i></a></li>
+                                                            <li><a href="https://twitter.com/Piyush_j_p/"><i class="fa fa-twitter"></i></a></li>
                                                             <li><a href=""><i class="fa fa-github"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                                            <li><a href="https://www.linkedin.com/in/piyush-jain-9b221b146"><i class="fa fa-linkedin"></i></a></li>
                                                         </ul>
                                                     </div>
                                                     <!-- END team-social-->
@@ -158,23 +165,44 @@
                                                     <img src="./img/juhi.jpg" alt="">
                                                     <div class="team-social">
                                                         <ul>
-                                                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
-                                                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                                            <li><a href="https://www.facebook.com/juhi.dadhich.7"><i class="fa fa-facebook"></i></a></li>
+                                                            <!--li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                                            <li><a href=""><i class="fa fa-github"></i></a></li-->
+                                                            <li><a href="https://www.linkedin.com/in/juhi-dadhich-b8647016b"><i class="fa fa-linkedin"></i></a></li>
                                                         </ul>
                                                     </div>
                                                     <!-- END team-social-->
                                                 </div>
                                                 <!-- END team-img-->
                                                 <div class="team-info text-center">
-                                                    <h4>Juhi</h4>
+                                                    <h4>Juhi Dadhich</h4>
                                                     <p>Internet of Things &amp; Web</p>
                                                 </div>
                                                 <!-- END team-info-->
                                             </div>
                                             <!-- END team-single-->
 
+                                            <div id="team-5" class="team-single">
+                                                <div class="team-img">
+                                                    <img src="./img/avinash.jpg" alt="">
+                                                    <div class="team-social">
+                                                        <ul>
+                                                            <li><a href="https://www.facebook.com/juhi.dadhich.7"><i class="fa fa-facebook"></i></a></li>
+                                                            <!--li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                                            <li><a href=""><i class="fa fa-github"></i></a></li-->
+                                                            <li><a href="https://www.linkedin.com/in/juhi-dadhich-b8647016b"><i class="fa fa-linkedin"></i></a></li>
+                                                        </ul>
+                                                    </div>
+                                                    <!-- END team-social-->
+                                                </div>
+                                                <!-- END team-img-->
+                                                <div class="team-info text-center">
+                                                    <h4>Avinash Verma</h4>
+                                                    <p>Digital Marketing &amp; Web</p>
+                                                </div>
+                                                <!-- END team-info-->
+                                            </div>
+                                            <!-- END team-single-->
 
                                         </div>
                                         <!-- END col-sm-8-->
