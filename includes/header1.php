c5afafd62e27c3f622081b79609e5066da745cd2
aakashrajput <aakashsingh1999@gmail.com>
2019-11-11 18:37:33 +0530

    projects menu

--- includes/header1.php
+++ includes/header1.php
@@ -49,6 +49,7 @@
                 <div class="sidebar-brand"><a href="./"><img src="img/logo.png" alt="Dashcore Template" class="logo"></a></div>
                 <ul class="nav navbar-nav ml-auto">
                   <li class="nav-item"><a class="nav-link" href="./">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./">Projects</a></li>
                     <li class="nav-item"><a class="nav-link" href="./teams.php">Team</a></li>
                     <li class="nav-item"><a class="nav-link" href="events.php">Events</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
